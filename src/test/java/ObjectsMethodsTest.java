6ea7c2d47195571ae932cb3cd5ebe04bedcbf963
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-12-05 19:35:38 +0100

    requireNonNullElseGet tests

--- src/test/java/ObjectsMethodsTest.java
+++ src/test/java/ObjectsMethodsTest.java
@@ -27,4 +27,28 @@ public class ObjectsMethodsTest {
     public void requireNonNullElse_nullObj_nullDefaultObj() {
         Objects.requireNonNullElse(null, null);
     }
+
+    @Test
+    public void requireNonNullElseGet_notNullObj() {
+        String a = Objects.requireNonNullElseGet("a", null);
+
+        assertThat(a, is("a"));
+    }
+
+    @Test
+    public void requireNonNullElseGet_nullObj_nonNullSupplier_nonNullSupplierValue() {
+        String b = Objects.requireNonNullElseGet(null, () -> "b");
+
+        assertThat(b, is("b"));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void requireNonNullElseGet_nullObj_nonNullSupplier_nullSupplierValue() {
+        Objects.requireNonNullElseGet(null, () -> null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void requireNonNullElseGet_nullObj_nullSupplier() {
+        Objects.requireNonNullElseGet(null, null);
+    }
 }
